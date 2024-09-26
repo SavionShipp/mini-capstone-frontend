37bda1cdd7bd2cdfc843fcd6ba7d7e1a9c70096d
SavionShipp <shippsavion209@yahoo.com>
2024-09-25 18:10:53 -0700

    added a index action

---
+++ src/ProductsIndex.jsx
@@ -0,0 +1,15 @@
+export function ProductsIndex(props) {
+  console.log(props.products)
+  return (
+    <div>
+      <h1>All Products</h1>
+      {props.products.map(product => (
+        <div key={product.id}>
+          <p>{product.name}</p>
+          <p>{product.price}</p>
+          <p>{product.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
--- src/ProductsPage.jsx
+++ src/ProductsPage.jsx
@@ -1,7 +1,24 @@
-export function PhotosPage() {
+import { ProductsIndex } from "./ProductsIndex"
+import { useState } from 'react'
+import axios from 'axios'
+
+export function ProductsPage() {
+  const [products, setProducts] = useState([])
+
+  const handleProductsIndex = () => {
+    console.log('hey')
+    axios.get("http://localhost:3000/products.json").then(response => {
+      console.log(response.data)
+      setProducts(response.data)
+    })
+  }
+
+
   return (
     <main>
       <h1>Welcome to React!</h1>
+      <ProductsIndex products={products}/>
+      <button onClick={handleProductsIndex}>button</button>
     </main>
-  )
+  );
 }
